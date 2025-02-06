047bccb07805ad8bc1c770752fae10f000857b72
JOABE DOS SANTOS COSTA <joabe_s_costa@estudante.sesisenai.org.br>
2025-02-05 22:00:29 -0300

    add

---
+++ revisao react/src/components/Body.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Body.css'
+import Demo1 from './Demo1'
+
+function Body() {
+  return (
+    <div className='container-body'>
+      <h1>AUTENTICAMENTE FALANDO</h1>
+      <Demo1 />
+    </div>
+  )
+}
+
+export default Body
--- revisao react/src/components/Header.jsx
+++ revisao react/src/components/Header.jsx
@@ -3,8 +3,11 @@ import './Header.css'
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className='container-header'>
+      <p>Logo</p>
+      <a href="#link1">Link1</a>
+      <a href="#link2">Link2</a>
+      <a href="#link3">Link3</a>
     </div>
   )
 }
